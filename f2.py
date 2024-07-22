ed35d15d65970411dd589053039a9b0f79d6f43d
gunturulokesh <161820935+gunturulokesh@users.noreply.github.com>
2024-07-22 18:32:28 +0530

    Update f2.py

--- f2.py
+++ f2.py
@@ -1,2 +1,17 @@
-print("Hello, World!")
+---
+- name: Install Nginx
+  hosts: web_servers
+  become: yes
+
+  tasks:
+    - name: Install Nginx
+      apt:
+        name: nginx
+        state: present
+        update_cache: yes
+
+    - name: Start Nginx service
+      service:
+        name: nginx
+        state: started
 
